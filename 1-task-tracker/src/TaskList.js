211f75f7363b72966a3d751d36042c84e09501d1
Aarón Fas <yo@aaron.com.es>
2023-12-01 21:35:57 +0100

    Add support for loading tasks asynchronously.
    - `<marquee>` were cool.

--- 1-task-tracker/src/TaskList.js
+++ 1-task-tracker/src/TaskList.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './TaskList.css'; // Make sure to import the CSS file
 
-const tasks = [
-  { id: 1, title: 'Task 1', completed: false },
-  { id: 2, title: 'Task 2', completed: true },
-  { id: 3, title: 'Task 3', completed: false },
-  // You can add more tasks here...
-];
 
 export default function TaskList() {
+  const [tasks, setTasks] = React.useState(null);
+
+  useEffect(() => {
+    // TODO Fetch tasks from the API.
+    setTimeout(() => {
+      setTasks([
+        { id: 1, title: 'Task 1', completed: false },
+        { id: 2, title: 'Task 2', completed: true },
+        { id: 3, title: 'Task 3', completed: false },
+        // You can add more tasks here...
+      ]);
+    }, Math.random() * 3000); // Simulate up to 3 seconds of delay.
+  }, []);
+
   return (
     <div className="task-list">
       <h2>Tasks</h2>
-      <ul>
+      {tasks && <ul>
         {tasks.map(task => (
           <li key={task.id} className={task.completed ? 'completed' : ''}>
             {task.title}
             <span className="status">{task.completed ? 'Done' : 'Pending'}</span>
           </li>
         ))}
-      </ul>
+      </ul> || <marquee>Loading...</marquee>}
     </div>
   );
 }
